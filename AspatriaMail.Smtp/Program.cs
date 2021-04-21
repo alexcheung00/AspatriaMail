3354388010887eb5069e3b43b0585fba76ccac1a
alexcheung00 <alexkhcheung00@gmail.com>
2021-04-21 11:20:18 +0800

    Add SMTP Project

---
+++ AspatriaMail.Smtp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspatriaMail.Smtp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
